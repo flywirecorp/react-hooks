52eb4c16101646cb82748eb7957024b5d781c506
Vicent Gozalbes <vigosan@gmail.com>
2020-06-23 08:56:38 +0200

    Update rollup configuration

--- rollup.config.js
+++ rollup.config.js
@@ -1,17 +1,26 @@
 import babel from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import pkg from './package.json';
 
 export default {
   input: 'src/index.js',
-  output: {
-    file: 'dist/bundle.js',
-    format: 'cjs',
-  },
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+    },
+  ],
   plugins: [
     resolve(),
     babel({
       exclude: 'node_modules/**',
     }),
+    commonjs(),
   ],
   external: ['react', 'react-dom'],
 };
