20778d117dbbe5db250e18eb336017855500e1f6
Vicent Gozalbes <vigosan@gmail.com>
2020-06-23 08:18:14 +0200

    esm

--- rollup.config.js
+++ rollup.config.js
@@ -1,16 +1,27 @@
 import babel from '@rollup/plugin-babel';
 import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import pkg from './package.json';
+
+const GLOBALS = {
+  react: 'React',
+  'react-dom': 'ReactDOM',
+};
+
+const EXTERNAL = ['react', 'react-dom'];
 
 export default {
   input: 'src/index.js',
   output: [
     {
-      file: 'dist/bundle.js',
+      file: pkg.main,
       format: 'cjs',
+      globals: GLOBALS,
+      external: EXTERNAL,
     },
     {
-      file: 'dist/bundle.es.js',
-      format: 'esm',
+      file: pkg.module,
+      format: 'es',
     },
   ],
   plugins: [
@@ -18,6 +29,7 @@ export default {
     babel({
       exclude: 'node_modules/**',
     }),
+    commonjs(),
   ],
   external: ['react', 'react-dom'],
 };
